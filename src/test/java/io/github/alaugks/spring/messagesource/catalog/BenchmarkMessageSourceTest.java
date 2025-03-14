33c8ebb2cb7dc8b66d5f1c06f8d96cc48ad280e5
André Laugks <al@pb82.de>
2025-03-14 15:46:18 +0100

    Add Apache License comment

--- src/test/java/io/github/alaugks/spring/messagesource/catalog/BenchmarkMessageSourceTest.java
+++ src/test/java/io/github/alaugks/spring/messagesource/catalog/BenchmarkMessageSourceTest.java
@@ -1,6 +1,20 @@
-package io.github.alaugks.spring.messagesource.catalog;
+/*
+ * Copyright 2024-2025 André Laugks <alaugks@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+package io.github.alaugks.spring.messagesource.catalog;
 
 import io.github.alaugks.spring.messagesource.catalog.catalog.CatalogInterface;
 import io.github.alaugks.spring.messagesource.catalog.records.TransUnit;
@@ -21,6 +35,8 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * This test compares the logic when resolving the code of CatalogMessageSourceBuilder vs. ResourceBundleMessageSource
