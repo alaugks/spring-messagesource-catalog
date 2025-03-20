ce6a29885049edd2c4771ec84a92a49e531696d3
André Laugks <al@pb82.de>
2025-03-20 13:11:47 +0100

    remove deprecated constructor

--- src/main/java/io/github/alaugks/spring/messagesource/catalog/resources/ResourcesLoader.java
+++ src/main/java/io/github/alaugks/spring/messagesource/catalog/resources/ResourcesLoader.java
@@ -37,20 +37,12 @@ public class ResourcesLoader {
 	private final List<String> fileExtensions;
 
 	public ResourcesLoader(Locale defaultLocale, LocationPattern locationPattern, List<String> fileExtensions) {
-		this(defaultLocale, locationPattern.getLocationPatterns(), fileExtensions);
-	}
-
-	/**
-	 *	Will replace with: ResourcesLoader(Locale defaultLocale, LocationPattern locationPattern, List<String> fileExtensions)
-	 */
-	@Deprecated(since = "0.6.0")
-	public ResourcesLoader(Locale defaultLocale, Set<String> locationPatterns, List<String> fileExtensions) {
 		Assert.notNull(defaultLocale, "Argument defaultLocale must not be null");
-		Assert.notNull(locationPatterns, "Argument locationPatterns must not be null");
+		Assert.notNull(locationPattern, "Argument locationPatterns must not be null");
 		Assert.notNull(fileExtensions, "Argument fileExtensions must not be null");
 
 		this.defaultLocale = defaultLocale;
-		this.locationPatterns = locationPatterns;
+		this.locationPatterns = locationPattern.getLocationPatterns();
 		this.fileExtensions = fileExtensions;
 	}
 
